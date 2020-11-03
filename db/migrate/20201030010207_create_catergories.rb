c70606c861ae0e0738ecdb991d0bdc9e6be1863f
arbrown110 <arbrown110@gmail.com>
2020-11-03 02:13:06 -0500

    rename for catergory.

--- db/migrate/20201030010207_create_catergories.rb
+++ db/migrate/20201030010207_create_catergories.rb
@@ -1,6 +1,6 @@
-class CreateCatergories < ActiveRecord::Migration[6.0]
+class CreateCategories < ActiveRecord::Migration[6.0]
   def change
-    create_table :catergories do |t|
+    create_table :categories do |t|
       t.string :name
 
       t.timestamps
